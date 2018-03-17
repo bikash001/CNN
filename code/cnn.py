9e2114b78b0081f38ec61883cc82c819c2274c18
Rahul Kejriwal <kejriwalrahul@outlook.com>
2018-03-17 15:48:44 +0530

    added cmdline argsparsing

--- code/cnn.py
+++ code/cnn.py
@@ -1,7 +1,41 @@
+# Python Lib Imports
 import numpy as np
+from argparse import ArgumentParser
+
+# Custom Imports
 import tensorflow as tf
 
+
+"""
+	Function to check if string is 1 or multiple of 5
+"""
+def mul_5(string):
+	val = int(string)
+	if val == 1 or val % 5 == 0:	return val
+	else:	raise ArgumentTypeError("%r is not a multiple of 5" % string)
+
+
+"""
+	Parses cmd line args
+"""
+def parse_cmd_args():
+
+	# Create Parser
+	parser = ArgumentParser()
+
+	# Add arguments
+	parser.add_argument("--lr", help="initial learning rate for gradient descent based algorithms", type=float, required=True)
+	parser.add_argument("--batch_size", help="batch size to be used - valid values are 1 and multiples of 5", type=mul_5, required=True)
+	parser.add_argument("--init", help="initialization method to be used - 1 for Xavier, 2 for He", type=int, choices=[1,2], required=True)
+	parser.add_argument("--save_dir", help="directory where pickled model should be stored", type=str, required=True)
+
+	# Parse args
+	args = parser.parse_args()
+	return args
+
+
 class CNN(object):
+
 	"""docstring for CNN"""
 	def __init__(self, arg):
 		super(CNN, self).__init__()
